c68433a44d0e5a103d2e86b084c763f5244c9df9
Vivek Sanghvi <viksipart@gmail.com>
2020-09-29 15:39:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,13 @@ count = 0
 while count < src.length do 
   inner_count = 0 
   while inner_count < src[count].length do 
-    if 
+    if src[count][inner_count].even?
+      puts src[count][inner_count]
+    end
+    inner_count += 1
+  end
+  count += 1
+end
       
     
     
